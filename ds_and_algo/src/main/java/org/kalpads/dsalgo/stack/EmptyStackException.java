52e6aa02f611bc11f44223c22de9abc3be5f5511
Kalpa Senanayake <senanayake.kalpa@gmail.com>
2019-05-27 20:34:51 +1000

    removeing old code

--- ds_and_algo/src/main/java/org/kalpads/dsalgo/stack/EmptyStackException.java
+++
@@ -1,7 +0,0 @@
-package org.kalpads.dsalgo.stack;
-
-/**
- * Created by kalpasenanayake on 6/6/17.
- */
-public class EmptyStackException extends Exception {
-}
--- ds_and_algo/src/main/java/org/kalpads/dsalgo/stack/StackFullException.java
+++
@@ -1,7 +0,0 @@
-package org.kalpads.dsalgo.stack;
-
-/**
- * Created by kalpasenanayake on 6/6/17.
- */
-public class StackFullException extends Exception {
-}
